c25eb3b2f4101ff6ee4e27fa79a658671083d97b
booky10 <boooky10@gmail.com>
2021-10-30 01:00:20 +0200

    updated the sit listener

--- src/main/java/tk/booky/craftattack/listener/SitListener.java
+++ src/main/java/tk/booky/craftattack/listener/SitListener.java
@@ -1,92 +1,102 @@
 package tk.booky.craftattack.listener;
 // Created by booky10 in CraftAttack (15:40 24.05.21)
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Tag;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.type.Slab;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.spigotmc.event.entity.EntityDismountEvent;
 import tk.booky.craftattack.CraftAttackMain;
 
-import java.util.Collection;
-
 public class SitListener implements Listener {
 
-    public SitListener() {
-        Bukkit.getScheduler().runTaskTimer(CraftAttackMain.main, () -> {
-            for (World world : Bukkit.getWorlds()) {
-                for (ArmorStand stand : world.getEntitiesByClass(ArmorStand.class)) {
-                    if (stand.getScoreboardTags().contains("chair") && stand.getPassengers().size() == 0) {
-                        stand.remove();
-                    }
-                }
-            }
-        }, 40, 40);
-    }
+    private static final NamespacedKey CHAIR_KEY = new NamespacedKey(CraftAttackMain.main, "chair");
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
-        if (!event.hasBlock() || !event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
-        if (event.getClickedBlock() == null || !Tag.STAIRS.isTagged(event.getClickedBlock().getType())) return;
-        if (event.getPlayer().isSneaking() || !event.getMaterial().isAir()) return;
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock() != null) {
+            if (!event.getPlayer().isSneaking() && event.getMaterial().isAir()) {
+                Block block = event.getClickedBlock();
+                Material material = block.getType();
+                byte type;
 
-        Location location = event.getClickedBlock().getLocation();
-        if (!location.clone().add(0, 1, 0).getBlock().getType().isAir()) return;
-        if (location.clone().subtract(0, 1, 0).getBlock().getType().isAir()) return;
-        location.add(0.5, 0.5, 0.5);
+                if (Tag.SLABS.isTagged(material)) {
+                    if (block.getBlockData() instanceof Slab slab && slab.getType() == Slab.Type.BOTTOM) {
+                        type = (byte) 1;
+                    } else {
+                        return;
+                    }
+                } else if (Tag.STAIRS.isTagged(material)) {
+                    type = (byte) 2;
+                } else {
+                    return;
+                }
 
-        if (location.getNearbyEntitiesByType(ArmorStand.class, 0.5, stand -> stand.getScoreboardTags().contains("chair")).size() > 0) return;
-        ArmorStand stand = location.getWorld().spawn(location, ArmorStand.class);
+                if (block.getRelative(BlockFace.UP).getType().isAir()) {
+                    Location location = block.getLocation().add(0.5, 0.3, 0.5);
+                    if (location.getNearbyEntitiesByType(ArmorStand.class, 0.5).size() == 0) {
+                        ArmorStand stand = location.getWorld().spawn(location, ArmorStand.class);
+                        PersistentDataContainer container = stand.getPersistentDataContainer();
+                        container.set(CHAIR_KEY, PersistentDataType.BYTE, type);
 
-        stand.addPassenger(event.getPlayer());
-
-        Bukkit.getScheduler().runTaskLater(CraftAttackMain.main, () -> {
-            stand.addScoreboardTag("chair");
-            stand.setInvisible(true);
-            stand.setMarker(true);
-            stand.setSmall(true);
-        }, 1);
+                        stand.addPassenger(event.getPlayer());
+                        stand.setInvisible(true);
+                        stand.setMarker(true);
+                        stand.setSmall(true);
+                    }
+                }
+            }
+        }
     }
 
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
-        Block block;
-        if (Tag.STAIRS.isTagged(event.getBlock().getType())) {
-            block = event.getBlock();
-        } else {
-            block = event.getBlock().getRelative(BlockFace.UP);
-            if (!Tag.STAIRS.isTagged(block.getType())) {
-                return;
+        Block block = event.getBlock();
+        Material material = block.getType();
+
+        if (Tag.STAIRS.isTagged(material) || Tag.SLABS.isTagged(material)) {
+            Location location = block.getLocation().add(0.5, 0.3, 0.5);
+            for (ArmorStand stand : location.getNearbyEntitiesByType(ArmorStand.class, 0.5)) {
+                if (stand.getPersistentDataContainer().has(CHAIR_KEY, PersistentDataType.BYTE)) {
+                    stand.remove();
+                }
             }
         }
-
-        Location location = block.getLocation().toCenterLocation();
-        Collection<ArmorStand> stands = location.getNearbyEntitiesByType(ArmorStand.class, 0.5, stand -> stand.getScoreboardTags().contains("chair"));
-        stands.forEach(ArmorStand::remove);
     }
 
     @EventHandler
     public void onDismount(EntityDismountEvent event) {
-        if (!(event.getDismounted() instanceof ArmorStand)) return;
-        if (!event.getDismounted().getScoreboardTags().contains("chair")) return;
+        Byte chairType = event.getDismounted().getPersistentDataContainer().get(CHAIR_KEY, PersistentDataType.BYTE);
+        if (chairType != null && event.getEntity() instanceof LivingEntity entity) {
+            Location location = event.getDismounted().getLocation().add(0, chairType / 2d, 0);
+            location.setPitch(entity.getLocation().getPitch());
+            location.setYaw(entity.getLocation().getYaw());
 
-        Location location = event.getDismounted().getLocation().clone().add(0, 2, 0);
-        location.setYaw(event.getEntity().getLocation().getYaw());
-        location.setPitch(event.getEntity().getLocation().getPitch());
-        event.getEntity().teleport(location);
+            if (location.clone().add(0, entity.getEyeHeight(), 0).getBlock().getType().isSolid()) {
+                event.getDismounted().getLocation().getBlock().breakNaturally(true);
 
-        Bukkit.getScheduler().runTaskLater(CraftAttackMain.main, () -> {
-            if (!event.getDismounted().isDead()) {
-                event.getDismounted().remove();
+                entity.teleport(entity.getLocation()
+                    .add(0, 1, 0)
+                    .toCenterLocation()
+                    .subtract(0, 0.375, 0)
+                );
+            } else {
+                entity.teleport(location);
             }
-        }, 5);
+
+            event.getDismounted().remove();
+        }
     }
 }
