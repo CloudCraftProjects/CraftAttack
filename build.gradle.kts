ec0c9113f028273c7edfb57eae73287aea3ee7a9
booky10 <boooky10@gmail.com>
2021-10-30 18:46:16 +0200

    rewrote the gradle buildscript in kts

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "tk.booky"
+version = "1.7.0"
+
+repositories {
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-public/")
+    maven("https://jitpack.io/")
+}
+
+dependencies {
+    api("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    api("dev.jorel.CommandAPI:commandapi-core:6.3.1")
+    api("com.mojang:brigadier:1.0.18")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifactId = project.name.toLowerCase()
+        from(components["java"])
+    }
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
