f4af31263466bb022c37d492ae8885267008afe1
booky10 <boooky10@gmail.com>
2023-06-11 01:39:24 +0200

    Update gradle wrapper to v8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
     id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
     id("xyz.jpenilla.run-paper") version "1.0.6"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "dev.booky"
