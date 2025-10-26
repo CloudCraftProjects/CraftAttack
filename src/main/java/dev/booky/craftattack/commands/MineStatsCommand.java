e001696fb47fe9f6520f4f2cf4f0d05ed7e98773
booky10 <boooky10@gmail.com>
2025-10-26 02:15:27 +0100

    Refactor scoreboard leaderboard creation into separate class

--- src/main/java/dev/booky/craftattack/commands/MineStatsCommand.java
+++ src/main/java/dev/booky/craftattack/commands/MineStatsCommand.java
@@ -4,30 +4,23 @@ package dev.booky.craftattack.commands;
 import com.destroystokyo.paper.profile.PlayerProfile;
 import dev.booky.craftattack.CaManager;
 import dev.booky.craftattack.utils.CaConfig;
+import dev.booky.craftattack.utils.LeaderboardUtil;
+import dev.booky.craftattack.utils.LeaderboardUtil.LeaderboardEntry;
+import dev.booky.craftattack.utils.LeaderboardUtil.LeaderboardResult;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.PlayerProfileArgument;
 import dev.jorel.commandapi.exceptions.WrapperCommandSyntaxException;
 import dev.jorel.commandapi.executors.CommandArguments;
 import dev.jorel.commandapi.executors.PlayerCommandExecutor;
-import it.unimi.dsi.fastutil.objects.Object2IntArrayMap;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.util.NumberConversions;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
 
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.Component.translatable;
@@ -50,21 +43,6 @@ public class MineStatsCommand extends CommandAPICommand implements PlayerCommand
 
         this.withPermission("craftattack.command.minestats." + objectiveName);
         this.executesPlayer(this);
-    }
-
-    private static LeaderboardEntry calcPlacedEntry(UnplacedLeaderboardEntry entry, LeaderboardEntry lastEntry) {
-        // calculate placement position
-        int placement;
-        if (lastEntry == null) {
-            placement = 1;
-        } else if (entry.score() == lastEntry.score()) {
-            // don't move one place further if still the same score
-            placement = lastEntry.place();
-        } else {
-            placement = lastEntry.place() + 1;
-        }
-
-        return entry.withPlacement(placement);
     }
 
     @Override
@@ -91,96 +69,15 @@ public class MineStatsCommand extends CommandAPICommand implements PlayerCommand
             return;
         }
 
-        // request all entries from scoreboard objective
-        Set<String> entryNames = scoreboard.getEntries();
-        Object2IntMap<OfflinePlayer> entries = new Object2IntArrayMap<>(entryNames.size());
-        for (String entryName : entryNames) {
-            OfflinePlayer target = Bukkit.getOfflinePlayerIfCached(entryName);
-            if (target == null) {
-                try {
-                    UUID.fromString(entryName);
-                    continue; // skip looking for player, is an entity
-                } catch (IllegalArgumentException ignored) {
-                    target = Bukkit.getOfflinePlayer(entryName);
-                }
-            }
+        LeaderboardResult leaderboard = LeaderboardUtil.buildLeaderboard(objective, LEADERBOARD_COUNT, sender);
 
-            if (!target.hasPlayedBefore()) {
-                continue; // unknown player
-            }
-
-            Score score = objective.getScore(target);
-            if (score.getScore() > 0) {
-                entries.put(target, score.getScore());
-            }
-        }
-
-        // extract self entry
-        UnplacedLeaderboardEntry unplacedSelfEntry = new UnplacedLeaderboardEntry(
-                sender, entries.getInt(sender));
-
-        // build sorted entries from raw entries
-        List<UnplacedLeaderboardEntry> unplacedEntries = entries.object2IntEntrySet().stream()
-                .sorted(Comparator.<Object2IntMap.Entry<OfflinePlayer>>comparingInt(Object2IntMap.Entry::getIntValue).reversed())
-                .map(entry -> new UnplacedLeaderboardEntry(entry.getKey(), entry.getIntValue()))
-                .toList();
-        int avgScore = unplacedEntries.stream()
-                .mapToInt(UnplacedLeaderboardEntry::score).average() // calc average
-                .stream().mapToInt(NumberConversions::round).findAny() // round to int
-                .orElse(0);
-
-        // add placement data to entries
-        List<LeaderboardEntry> leaderboard = new ArrayList<>(LEADERBOARD_COUNT);
-        LeaderboardEntry lastEntry = null;
-        LeaderboardEntry selfEntry = null;
-        for (UnplacedLeaderboardEntry entry : unplacedEntries) {
-            LeaderboardEntry placedEntry = calcPlacedEntry(entry, lastEntry);
-            // add to leaderboard if not already full
-            if (leaderboard.size() < LEADERBOARD_COUNT) {
-                leaderboard.add(placedEntry);
-            }
-
-            lastEntry = placedEntry;
-            if (entry.equals(unplacedSelfEntry)) {
-                selfEntry = placedEntry;
-            }
-
-            if (leaderboard.size() >= LEADERBOARD_COUNT && selfEntry != null) {
-                break; // early cancel, all necessary data has already been calculated
-            }
-        }
-
-        // fallback self entry if not on leaderboard
-        if (selfEntry == null) {
-            int placement = lastEntry == null ? 1 : lastEntry.place() + 1;
-            selfEntry = unplacedSelfEntry.withPlacement(placement);
-        }
-
-        // finally, print results to command executor
         sender.sendMessage(CaManager.getPrefix().append(translatable(
                 "ca.command.mine-stats.leaderboard.header", NamedTextColor.GREEN,
-                text(this.objectiveName), text(entries.size()), text(avgScore))));
-        for (LeaderboardEntry entry : leaderboard) {
-            sender.sendMessage(CaManager.getPrefix().append(entry.buildMessage()));
-        }
-        // show own place for players which aren't in the top 10
-        if (!leaderboard.contains(selfEntry)) {
-            sender.sendMessage(CaManager.getPrefix().append(selfEntry.buildMessage()));
-        }
-    }
-
-    private record UnplacedLeaderboardEntry(OfflinePlayer player, int score) {
-
-        public LeaderboardEntry withPlacement(int place) {
-            return new LeaderboardEntry(this.player, place, this.score);
-        }
-    }
-
-    private record LeaderboardEntry(OfflinePlayer player, int place, int score) {
-
-        public Component buildMessage() {
-            return translatable("ca.command.mine-stats.leaderboard.entry", NamedTextColor.GREEN,
-                    text(this.place), text(String.valueOf(this.player.getName())), text(this.score));
+                text(this.objectiveName),
+                text(leaderboard.entries().size()),
+                text(leaderboard.average()))));
+        for (LeaderboardEntry entry : leaderboard.entries()) {
+            sender.sendMessage(CaManager.getPrefix().append(entry.buildLine()));
         }
     }
 }
---
+++ src/main/java/dev/booky/craftattack/utils/LeaderboardUtil.java
@@ -0,0 +1,142 @@
+package dev.booky.craftattack.utils;
+// Created by booky10 in CraftAttack (02:58 26.10.2025)
+
+import it.unimi.dsi.fastutil.objects.Object2IntArrayMap;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.util.NumberConversions;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.Component.translatable;
+
+@NullMarked
+public final class LeaderboardUtil {
+
+    private LeaderboardUtil() {
+    }
+
+    private static LeaderboardEntry calcPlacedEntry(UnplacedLeaderboardEntry entry, @Nullable LeaderboardEntry lastEntry) {
+        // calculate placement position
+        int placement;
+        if (lastEntry == null) {
+            placement = 1;
+        } else if (entry.score() == lastEntry.score()) {
+            // don't move one place further if still the same score
+            placement = lastEntry.place();
+        } else {
+            placement = lastEntry.place() + 1;
+        }
+
+        return entry.withPlacement(placement);
+    }
+
+    public static LeaderboardResult buildLeaderboard(Objective objective, int size, @Nullable OfflinePlayer self) {
+        Scoreboard scoreboard = objective.getScoreboard();
+        if (scoreboard == null) {
+            throw new IllegalArgumentException("Objective " + objective + " has no assigned scoreboard, can't build leaderboard");
+        }
+        // request all entries from scoreboard objective
+        Set<String> entryNames = scoreboard.getEntries();
+        Object2IntMap<OfflinePlayer> entries = new Object2IntArrayMap<>(entryNames.size());
+        for (String entryName : entryNames) {
+            OfflinePlayer target = Bukkit.getOfflinePlayerIfCached(entryName);
+            if (target == null) {
+                try {
+                    UUID.fromString(entryName);
+                    continue; // skip looking for player, is an entity
+                } catch (IllegalArgumentException ignored) {
+                    target = Bukkit.getOfflinePlayer(entryName);
+                }
+            }
+
+            if (!target.hasPlayedBefore()) {
+                continue; // unknown player
+            }
+
+            Score score = objective.getScore(target);
+            if (score.getScore() > 0) {
+                entries.put(target, score.getScore());
+            }
+        }
+
+        // extract self entry
+        UnplacedLeaderboardEntry unplacedSelfEntry = self == null ? null :
+                new UnplacedLeaderboardEntry(self, entries.getInt(self));
+
+        // build sorted entries from raw entries
+        List<UnplacedLeaderboardEntry> unplacedEntries = entries.object2IntEntrySet().stream()
+                .sorted(Comparator.<Object2IntMap.Entry<OfflinePlayer>>comparingInt(Object2IntMap.Entry::getIntValue).reversed())
+                .map(entry -> new UnplacedLeaderboardEntry(entry.getKey(), entry.getIntValue()))
+                .toList();
+        int avgScore = unplacedEntries.stream()
+                .mapToInt(UnplacedLeaderboardEntry::score).average() // calc average
+                .stream().mapToInt(NumberConversions::round).findAny() // round to int
+                .orElse(0);
+
+        // add placement data to entries
+        List<LeaderboardEntry> leaderboard = new ArrayList<>(size);
+        LeaderboardEntry lastEntry = null;
+        LeaderboardEntry selfEntry = null;
+        for (UnplacedLeaderboardEntry entry : unplacedEntries) {
+            LeaderboardEntry placedEntry = calcPlacedEntry(entry, lastEntry);
+            // add to leaderboard if not already full
+            if (leaderboard.size() < size) {
+                leaderboard.add(placedEntry);
+            }
+
+            lastEntry = placedEntry;
+            if (entry.equals(unplacedSelfEntry)) {
+                selfEntry = placedEntry;
+            }
+
+            if (leaderboard.size() >= size && (selfEntry != null || unplacedSelfEntry == null)) {
+                break; // early cancel, all necessary data has already been calculated
+            }
+        }
+
+        // fallback self entry if not on leaderboard
+        if (selfEntry == null && unplacedSelfEntry != null) {
+            int placement = lastEntry == null ? 1 : lastEntry.place() + 1;
+            selfEntry = unplacedSelfEntry.withPlacement(placement);
+        }
+
+        // add self entry to bottom of leaderboard
+        if (selfEntry != null && !leaderboard.contains(selfEntry)) {
+            leaderboard.add(selfEntry);
+        }
+
+        return new LeaderboardResult(leaderboard, avgScore);
+    }
+
+    private record UnplacedLeaderboardEntry(OfflinePlayer player, int score) {
+
+        public LeaderboardEntry withPlacement(int place) {
+            return new LeaderboardEntry(this.player, place, this.score);
+        }
+    }
+
+    public record LeaderboardEntry(OfflinePlayer player, int place, int score) {
+
+        public Component buildLine() {
+            return translatable("ca.command.mine-stats.leaderboard.entry", NamedTextColor.GREEN,
+                    text(this.place), text(String.valueOf(this.player.getName())), text(this.score));
+        }
+    }
+
+    public record LeaderboardResult(List<LeaderboardEntry> entries, int average) {
+    }
+}
